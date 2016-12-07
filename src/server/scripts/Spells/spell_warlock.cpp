64565a7573aba97d7bc222783f3db39fa6ab13e4
home-pc\pak3935 <pak3935@gmail.com>
2016-12-07 23:53:29 +0300

    Core/Spell: Demonic Circle Remove Auras Root

--- src/server/scripts/Spells/spell_warlock.cpp
+++ src/server/scripts/Spells/spell_warlock.cpp
@@ -1068,7 +1068,7 @@ class spell_warl_demonic_circle_teleport : public SpellScriptLoader
                     if (GameObject* circle = player->GetGameObject(SPELL_WARLOCK_DEMONIC_CIRCLE_SUMMON))
                     {
                         player->NearTeleportTo(circle->GetPositionX(), circle->GetPositionY(), circle->GetPositionZ(), circle->GetOrientation(), false, false, false, true);
-                        player->RemoveAurasWithMechanic(1<<MECHANIC_SNARE);
+						player->RemoveAurasWithMechanic(1 << MECHANIC_ROOT);
                     }
                 }
             }
